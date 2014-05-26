97b27f942b7f9404481c713cf7af28517dc48583
Ibrahim Cihan DIKMEN <chndkmn@hotmail.com>
2014-05-26 10:48:22 +0300

    fonksiyon.php

---
+++ fonksiyon.php
@@ -0,0 +1,64 @@
+<?php
+
+function fotografMeta($imagePath) {
+
+    if ((isset($imagePath)) and (file_exists($imagePath))) {
+   
+      $exif_ifd0 = read_exif_data($imagePath ,'IFD0' ,0);      
+      $exif_exif = read_exif_data($imagePath ,'EXIF' ,0);
+     
+      $notFound = "Unavailable";
+     
+      // Make
+      if (@array_key_exists('Make', $exif_ifd0)) {
+        $camMake = $exif_ifd0['Make'];
+      } else { $camMake = $notFound; }
+     
+      // Model
+      if (@array_key_exists('Model', $exif_ifd0)) {
+        $camModel = $exif_ifd0['Model'];
+      } else { $camModel = $notFound; }
+     
+      // Exposure
+      if (@array_key_exists('ExposureTime', $exif_ifd0)) {
+        $camExposure = $exif_ifd0['ExposureTime'];
+      } else { $camExposure = $notFound; }
+
+      // Aperture
+      if (@array_key_exists('ApertureFNumber', $exif_ifd0['COMPUTED'])) {
+        $camAperture = $exif_ifd0['COMPUTED']['ApertureFNumber'];
+      } else { $camAperture = $notFound; }
+     
+      // Date
+      if (@array_key_exists('DateTime', $exif_ifd0)) {
+        $camDate = $exif_ifd0['DateTime'];
+      } else { $camDate = $notFound; }
+     
+      // ISO
+      if (@array_key_exists('ISOSpeedRatings',$exif_exif)) {
+        $camIso = $exif_exif['ISOSpeedRatings'];
+      } else { $camIso = $notFound; }
+     
+      $return = array();
+      $return['make'] = $camMake;
+      $return['model'] = $camModel;
+      $return['exposure'] = $camExposure;
+      $return['aperture'] = $camAperture;
+      $return['date'] = $camDate;
+      $return['iso'] = $camIso;
+      return $return;
+   
+    } else {
+      return false;
+    }
+}
+
+
+$fotograf = fotografMeta("/img/myphoto.jpg");
+echo "Used: " . $fotograf['make'] . " " . $fotograf['model'] . "<br />";
+echo "Exposure Time: " . $fotograf['exposure'] . "<br />";
+echo "Aperture: " . $fotograf['aperture'] . "<br />";
+echo "ISO: " . $fotograf['iso'] . "<br />";
+echo "Date Taken: " . $fotograf['date'] . "<br />";
+
+?>
